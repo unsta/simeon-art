b862f713f95213a31268b723aa2bd61979b9c5b8
Desislav Savov <desislav.savov@axsmarine.com>
2017-11-22 11:06:41 +0200

    Adds contacts page.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
